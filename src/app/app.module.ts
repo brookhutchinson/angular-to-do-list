ac9c3c6dcb6c8b039c8ca33dbf70157d0f7e3f79
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-03-03 10:19:34 -0600

    Register HttpClientModule with AppModule Import HttpClientModule into AppModule Add HttpClientModule to AppModule @NgModule "imports" property array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@
 import { NgModule }         from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule }    from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 // components
 import { AppComponent }     from './app.component';
 
 @NgModule({
   // modules
-  imports: [ AppRoutingModule, BrowserModule ],
+  imports: [ AppRoutingModule, BrowserModule, HttpClientModule ],
   // components
   declarations: [ AppComponent ],
   // services
