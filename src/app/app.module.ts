7341071855facd85f3d44dea323130c0f4004198
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-03-03 10:36:55 -0600

    Register FormsModule with AppModule Import FormsModule into AppModule Add FormsModule to AppModule @NgModule "imports" property array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { NgModule }         from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule }    from '@angular/platform-browser';
+import { FormsModule }      from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 // components
@@ -9,7 +10,7 @@ import { AppComponent }     from './app.component';
 
 @NgModule({
   // modules
-  imports: [ AppRoutingModule, BrowserModule, HttpClientModule ],
+  imports: [ AppRoutingModule, BrowserModule, FormsModule, HttpClientModule ],
   // components
   declarations: [ AppComponent ],
   // services
